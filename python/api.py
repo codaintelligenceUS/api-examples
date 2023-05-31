34bab40882c1b7baa2be50f93ffb8021c020384c
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-05-31 23:14:22 +0300

    feat(python): Add start scan function

--- python/api.py
+++ python/api.py
@@ -18,8 +18,6 @@ print(f"🔧 Config: {config}")
 def create_registration():
     """
     This function performs the create co-managed registration call.
-    Brief explanations of the keys are provided.
-    Check the OpenAPI definition for more information.
     """
     print("🌟 Creating new registration...")
     response = requests.put(
@@ -55,5 +53,55 @@ def create_registration():
     return response.json()
 
 
+def get_cloud_scanner_id(tenant_id):
+    """Retrieves the ID of the default cloud scanner"""
+
+tenant_id_headers = {**headers, "FootprintTenantId": str(tenant_id)}
+    print("📡 Retrieving cloud scanner...")
+    scanners_response = requests.get(
+        f"{BASE_URL}/api/console/scanSurface/scanners/", headers=tenant_id_headers
+    )
+    cloud_scanner_entry = [
+        scanner
+        for scanner in scanners_response.json()
+        if scanner["isDefaultCloudScanner"]
+    ][0]
+
+    return cloud_scanner_entry["id"]
+
+
+def start_scan(tenant_id):
+    """
+    This function performs the start scan call.
+    """
+
+    tenant_id_headers = {**headers, "FootprintTenantId": str(tenant_id)}
+    scanner_id = get_cloud_scanner_id(tenant_id)
+    print("🏃 Starting scan...")
+    response = requests.post(
+        f"{BASE_URL}/api/console/scanSurface/",
+        headers=tenant_id_headers,
+        data=json.dumps(
+            {
+                "scanTargets": [config["FOOTPRINT_SCAN_TARGET"]],
+                "scanners": [scanner_id],
+                "credentialsId": 0,
+            }
+        ),
+    )
+    print(f"✅ Started scan: {response.status_code} {response.json()}")
+
+
+def wait_for_scan_finish(tenant_id):
+    """
+    Polls the server until thesc
+    an is done"""
+
+
+
+
+
+
 if __name__ == "__main__":
-    registration_id = create_registration()["id"]
+    tenant_id = 536  # create_registration()["meta"]["tenantId"]
+    start_scan(tenant_id)
